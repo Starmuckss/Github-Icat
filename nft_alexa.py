33230c84ebca523191845a6b1b753b0f96973c03
Sefa Yapıcı <mustafasefayapici@gmail.com>
2021-10-03 15:06:26 +0300

    Update nft_alexa.py

--- nft_alexa.py
+++ nft_alexa.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 10 21:36:08 2021
-
+Find Alexa Rank of series in cryptoslam with web scraping
 @author: HP
 """
 from selenium import webdriver
@@ -22,12 +21,18 @@ import numpy as np
 
 # Problem gathering data
 def get_nft_web_pages(opensea_series):
+    """
+    Find homepages of 600 opensea NFT series
+    Homepage links are given at opensea product pages for eg:
+    https://opensea.io/collection/boredapeyachtclub one of the buttons at the top right of the
+    page redirects to the offical webpage of boredapeyacthclub, which is required for alexa rank
+    """
     webpages_list = list()
+    options = webdriver.FirefoxOptions()
+    # options.headless = True
+    options.add_argument("--start-maximized")
+    browser = webdriver.Firefox(options=options)
     for url in opensea_series["Serie_link"]:
-        options = webdriver.FirefoxOptions()
-        # options.headless = True
-        options.add_argument("--start-maximized")
-        browser = webdriver.Firefox(options=options)
         try:
             browser.get(url)
         except TimeoutException as e:
@@ -45,16 +50,19 @@ def get_nft_web_pages(opensea_series):
     
     browser.close()
     opensea_series["Serie_link"] = webpages_list
-    
-opensea_series = pd.read_excel("opensea_series.xlsx")
+    opensea_series.to_excel("webpages_for_alexa.xlsx")
 
-webpages_list = list()
-
-webpages_for_alexa = pd.read_excel("webpages_for_alexa.xlsx")
-
-        
-opensea_series = pd.read_excel("opensea_series.xlsx")
+opensea_series = pd.read_excel("opensea_series.xlsx") # Süleyman provided this file
+# If we have it, read it and carry on. If not, obtain links for webpages
+try:
+    webpages_for_alexa = pd.read_excel("webpages_for_alexa.xlsx")
+except FileNotFoundError:
+    get_nft_web_pages(opensea_series)
+    webpages_for_alexa = pd.read_excel("webpages_for_alexa.xlsx")
+     
 true_alexa_ranks = list()
+
+# Get Alexa Rank from alexa.com
 for homepage in webpages_for_alexa["webpages"]:
     if isinstance(homepage , float):
         true_alexa_ranks.append("")
@@ -83,6 +91,7 @@ for homepage in webpages_for_alexa["webpages"]:
     except:
         print("No global rank found for ", site_name)
         true_alexa_ranks.append("")
+
 webpages_for_alexa["Alexa Rank"] = true_alexa_ranks 
 webpages_for_alexa.to_excel("alexa_ranks.xlsx")       
         # Display country rank(s) Not needed for now
